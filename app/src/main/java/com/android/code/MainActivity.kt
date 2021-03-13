5cb411f86dab400cb59fda790d666bfe6338a3a7
AdamRight <36788619+AdamRight@users.noreply.github.com>
2021-03-13 23:16:55 +0800

    constraintlayout

--- app/src/main/java/com/android/code/MainActivity.kt
+++ app/src/main/java/com/android/code/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.android.code
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.code.constraintlayout.ConstraintLayoutActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mConstraintLayout.setOnClickListener {
+            startActivity(Intent(this, ConstraintLayoutActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/android/code/constraintlayout/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/android/code/constraintlayout/ConstraintLayoutActivity.kt
@@ -2,11 +2,31 @@ package com.android.code.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.TransitionManager
 import com.android.code.R
+import kotlinx.android.synthetic.main.activity_circular_reveal.*
 
 class ConstraintLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint_layout)
+
+        //setContentView(R.layout.activity_constraint_layout)
+        setContentView(R.layout.activity_circular_reveal)
+
+        favorite.setOnClickListener {
+            placeholder.setContentId(R.id.favorite)
+        }
+
+        changeView.setOnClickListener {
+            val constraintSet = ConstraintSet().apply {
+                isForceId = false
+                clone(this@ConstraintLayoutActivity,
+                    R.layout.activity_circular_reveal2
+                )
+            }
+            TransitionManager.beginDelayedTransition(parentBegin)
+            constraintSet.applyTo(parentBegin)
+        }
     }
 }
