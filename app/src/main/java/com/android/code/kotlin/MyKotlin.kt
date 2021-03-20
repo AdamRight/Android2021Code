de37ef5b6f01df6b4ac666ef3cd8f9159e817fc7
AdamRight <36788619+AdamRight@users.noreply.github.com>
2021-03-20 20:00:14 +0800

    kotlin构造

--- app/src/main/java/com/android/code/kotlin/MyKotlin.kt
+++ app/src/main/java/com/android/code/kotlin/MyKotlin.kt
@@ -9,4 +9,15 @@ fun main() {
     Util2.int2Double(3)
 
     Util3.int2Double(4)
+
+    var user = User1()
+    var myName = user.name ?: "kotlin"
+
+    if (user.name?.length ?: 0 < 4){
+
+    }
+
+
+    var arrayListUser1 = ArrayList<User1>()
+    var arrayListUser2: List<User1> = arrayListUser1.filter { it.name == "kotlin" }
 }
---
+++ app/src/main/java/com/android/code/kotlin/User1.kt
@@ -0,0 +1,9 @@
+package com.android.code.kotlin
+
+/**
+ * Created by jiangtea on 2021/3/20.
+ *
+ * 主构造函数
+ */
+class User1 constructor(var name: String? = null, var password: String? = null) {
+}
